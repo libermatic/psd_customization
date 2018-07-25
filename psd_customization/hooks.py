c969d2c936f108731238b4a778dda30358afe9dc
Sun Howwrongbum <sun@libermatic.com>
2018-07-25 23:01:58 +0530

    Add custom script to set naming series

--- psd_customization/hooks.py
+++ psd_customization/hooks.py
@@ -20,7 +20,7 @@ fixtures = [
             'Batch-batch_id-default',
             'Batch-expiry_date-bold',
         ]]],
-    }
+    },
 ]
 
 # Includes in <head>
@@ -28,7 +28,7 @@ fixtures = [
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/psd_customization/css/psd_customization.css"
-# app_include_js = "/assets/psd_customization/js/psd_customization.js"
+app_include_js = "/assets/js/psd_customization.min.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/psd_customization/css/psd_customization.css"
---
+++ psd_customization/public/js/naming_series.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2018, Libermatic and contributors
+// For license information, please see license.txt
+
+/**
+ * Sets naming_series of the DocType based on Company abbr.
+ * This happens only when naming_series for the applicable DocTypes are set as
+ * per the schema below.
+ */
+
+async function set_naming_series(frm, dt) {
+  const { message: company } = await frappe.db.get_value(
+    'Company',
+    frm.doc['company'],
+    'abbr'
+  );
+  const renaming_doctypes = {
+    'Sales Invoice': `SI-${company['abbr']}/.YY.-.#####`,
+    'Payment Entry': `PE-${company['abbr']}/.YY.-.#####`,
+  };
+  const naming_series = frm.get_docfield('naming_series').options;
+  if (naming_series.indexOf(renaming_doctypes[dt]) > -1) {
+    frm.set_value('naming_series', renaming_doctypes[dt]);
+  }
+}
+
+frappe.ui.form.on('Sales Invoice', { company: set_naming_series });
+frappe.ui.form.on('Payment Entry', { company: set_naming_series });
