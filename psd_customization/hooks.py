bb44428fddfdd64ee08614adc6c79729fd2dd78c
Sun Howwrongbum <sun@libermatic.com>
2018-08-15 23:43:34 +0530

    Gym Member: make quick entry

--- psd_customization/hooks.py
+++ psd_customization/hooks.py
@@ -32,7 +32,10 @@ fixtures = [
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/psd_customization/css/psd_customization.css"
-app_include_js = "/assets/psd_customization/js/naming_series.js"
+app_include_js = [
+    '/assets/psd_customization/js/naming_series.js',
+    '/assets/psd_customization/js/gym_member_quick_entry.js',
+]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/psd_customization/css/psd_customization.css"
