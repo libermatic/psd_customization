3167e406b8f8c532dab600fb5a6be9e94355b23e
Sun Howwrongbum <sun@libermatic.com>
2018-08-19 22:12:20 +0530

    Gym Member: handle linking contacts and address

--- psd_customization/fitness_world/doctype/gym_member/gym_member.js
+++ psd_customization/fitness_world/doctype/gym_member/gym_member.js
@@ -3,19 +3,76 @@
 
 frappe.ui.form.on('Gym Member', {
   refresh: function(frm) {
+    frm.set_query('emergency_contact', function(doc) {
+      return {
+        query:
+          'psd_customization.fitness_world.api.gym_member.get_member_contacts',
+        filters: { member: doc.name },
+      };
+    });
     frappe.dynamic_link = {
       doc: frm.doc,
       fieldname: 'name',
       doctype: 'Gym Member',
     };
     frm.toggle_display(
-      ['contact_section', 'address_html', 'contact_html'],
+      ['contact_section', 'address_html', 'contact_html', 'emergency_contact'],
       !frm.doc.__islocal
     );
+    frm.toggle_enable('customer', frm.doc.__islocal);
     if (!frm.doc.__islocal) {
-      frappe.contacts.render_address_and_contact(frm);
+      frm.trigger('render_address_and_contact');
     } else {
       frappe.contacts.clear_address_and_contact(frm);
     }
+  },
+  render_address_and_contact: function(frm) {
+    frappe.contacts.render_address_and_contact(frm);
+    function make_dialog(doctype) {
+      return new frappe.ui.Dialog({
+        title: `Select ${doctype}`,
+        fields: [{ fieldname: 'docname', fieldtype: 'Link', options: doctype }],
+        primary_action: async function() {
+          await frappe.call({
+            method:
+              'psd_customization.fitness_world.api.gym_member.link_member_to_doctype',
+            args: {
+              member: frm.doc['name'],
+              doctype,
+              docname: this.get_value('docname'),
+            },
+          });
+          frm.reload_doc();
+          this.hide();
+        },
+      });
+    }
+
+    const address_dialog = make_dialog('Address');
+    const $link_btn_address = $(
+      '<button class="btn btn-xs btn-default btn-link-address">Link Address</button>'
+    ).on('click', function() {
+      address_dialog.show();
+    });
+    $(frm.fields_dict['address_html'].wrapper)
+      .find('.btn-address')
+      .after($link_btn_address);
+
+    const contact_dialog = make_dialog('Contact');
+    const $link_btn_contact = $(
+      '<button class="btn btn-xs btn-default btn-link-contact">Link Contact</button>'
+    ).on('click', function() {
+      contact_dialog.show();
+    });
+    $(frm.fields_dict['contact_html'].wrapper)
+      .find('.btn-contact')
+      .unbind('click')
+      .on('click', function() {
+        frappe.new_doc('Contact', {
+          first_name: frm.doc['first_name'],
+          last_name: frm.doc['last_name'],
+        });
+      })
+      .after($link_btn_contact);
   },
 });
