5004475116e08ddaf9b16e20df3ce769aa2c6465
Sun Howwrongbum <sun@libermatic.com>
2018-08-27 17:40:42 +0530

    Gym Membership: create this

---
+++ psd_customization/fitness_world/doctype/gym_member/gym_member_dashboard.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Libermatic and contributors
+# For license information, please see license.txt
+
+
+def get_data():
+    return {
+        'heatmap': True,
+        'transactions': [
+            {
+                'label': 'Reference',
+                'items': ['Gym Membership'],
+                'fieldname': 'member',
+            },
+        ],
+    }
