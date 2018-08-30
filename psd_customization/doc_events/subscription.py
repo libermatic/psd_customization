beba7049c8fcd63b216d829944ac7d0a65d90c9e
Sun Howwrongbum <sun@libermatic.com>
2018-08-30 18:03:07 +0530

    Gym Membership: update form to display only member and subscription
    - handle setting missing fields
    - fix error during Subscription update

--- psd_customization/doc_events/subscription.py
+++ psd_customization/doc_events/subscription.py
@@ -3,39 +3,28 @@ from __future__ import unicode_literals
 import frappe
 from frappe.utils import add_days
 
+from psd_customization.fitness_world.doctype.gym_membership.gym_membership \
+    import status_map
+
 
 def on_submit(doc, method):
     if doc.reference_gym_member:
         membership = frappe.get_doc({
             'doctype': 'Gym Membership',
             'member': doc.reference_gym_member,
-            'status': 'Active',
-            'start_date': doc.start_date,
-            'end_date': doc.end_date,
-            'frequency': doc.frequency,
             'subscription': doc.name,
         })
-        si = frappe.get_doc('Sales Invoice', doc.reference_document)
-        if si:
-            for item in si.items:
-                membership.append('items', {
-                    'item_code': item.item_code,
-                    'item_name': item.item_name,
-                })
         membership.insert()
         membership.submit()
 
 
 def on_update_after_submit(doc, method):
-    status_map = {
-        'Submitted': 'Active',
-        'Cancelled': 'Cancelled',
-    }
     if doc.reference_gym_member:
-        membership = frappe.get_doc(
-            'Gym membership', filters={'subscription': doc.name}
+        membership_name = frappe.db.get_value(
+            'Gym Membership', filters={'subscription': doc.name}
         )
-        if membership:
+        if membership_name:
+            membership = frappe.get_doc('Gym Membership', membership_name)
             membership.status = status_map.get(doc.status, membership.status)
             membership.expiry_date = add_days(doc.next_schedule_date, -1)
             membership.save()
--- psd_customization/fitness_world/doctype/gym_membership/gym_membership.py
+++ psd_customization/fitness_world/doctype/gym_membership/gym_membership.py
@@ -7,5 +7,31 @@ import frappe
 from frappe.model.document import Document
 
 
+status_map = {
+    'Submitted': 'Active',
+    'Stopped': 'Stopped',
+    'Cancelled': 'Cancelled',
+    'Completed': 'Stopped',
+}
+
+
 class GymMembership(Document):
-    pass
+    def before_submit(self):
+        subscription = frappe.get_doc('Subscription', self.subscription)
+        if subscription.docstatus != 1:
+            return frappe.throw(
+                'Subscription {} has a status of {}'.format(
+                    self.subscription, subscription.status
+                )
+            )
+        self.status = status_map.get(subscription.status)
+        self.start_date = subscription.start_date
+        self.end_date = subscription.end_date
+        self.frequency = subscription.frequency
+        si = frappe.get_doc('Sales Invoice', subscription.reference_document)
+        if si:
+            for item in si.items:
+                self.append('items', {
+                    'item_code': item.item_code,
+                    'item_name': item.item_name,
+                })
