dcdac4b74071fd0388a32da87f567f54599f6cdd
Sun Howwrongbum <sun@libermatic.com>
2018-09-02 14:21:22 +0530

    Refactor Sales Invoice and Subscription hooks
    - add more methods to Gym Membership
    - add api method for Gym Membership

--- psd_customization/doc_events/sales_invoice.py
+++ psd_customization/doc_events/sales_invoice.py
@@ -1,64 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import add_days, add_months, add_years, formatdate
+from frappe.utils import formatdate
 from frappe.core.doctype.sms_settings.sms_settings \
     import get_contact_number, send_sms
 
 from psd_customization.fitness_world.doctype.gym_sms_template.\
     gym_sms_template import get_sms_text
+from psd_customization.fitness_world.doctype.gym_membership.\
+    gym_membership import get_expiry_date, is_gym_customer
+from psd_customization.fitness_world.api.gym_membership \
+    import get_membership_by_subscription
 
 
 def _make_template_data(doc, frequency):
-    def get_period():
-        if frequency == 'Daily':
-            return doc.get_formatted('posting_date')
-        if frequency == 'Weekly':
-            return '{} - {}'.format(
-                doc.get_formatted('posting_date'),
-                formatdate(add_days(doc.posting_date, 6))
-            )
-        if frequency == 'Monthly':
-            return '{} - {}'.format(
-                doc.get_formatted('posting_date'),
-                formatdate(add_days(add_months(doc.posting_date, 1), -1))
-            )
-        if frequency == 'Quaterly':
-            return '{} - {}'.format(
-                doc.get_formatted('posting_date'),
-                formatdate(add_days(add_months(doc.posting_date, 3), -1))
-            )
-        if frequency == 'Half-Yearly':
-            return '{} - {}'.format(
-                doc.get_formatted('posting_date'),
-                formatdate(add_days(add_months(doc.posting_date, 6), -1))
-            )
-        if frequency == 'Yearly':
-            return '{} - {}'.format(
-                doc.get_formatted('posting_date'),
-                formatdate(add_years(doc.posting_date, 1))
-            )
-        return None
+    end_date = get_expiry_date(doc.posting_date, frequency)
+    period = doc.get_formatted('posting_date') \
+        if doc.posting_date == end_date else \
+        '{} - {}'.format(
+            doc.get_formatted('posting_date'), formatdate(end_date)
+        )
     return {
         'customer_name': doc.customer_name,
         'sales_invoice': doc.name,
         'amount': doc.get_formatted('rounded_total'),
-        'period': get_period(),
+        'period': period,
         'due_date': doc.get_formatted('due_date'),
     }
 
 
 def on_submit(doc, method):
-    if doc.subscription:
+    if is_gym_customer(doc.customer) and doc.subscription:
+        membership = get_membership_by_subscription(doc.subscription)
+        frequency = frappe.db.get_value(
+            'Subscription', doc.subscription, 'frequency'
+        )
+        if doc.status == 'Paid' and membership:
+            membership.update_expiry_date(
+                get_expiry_date(doc.posting_date, frequency)
+            )
+
         if doc.contact_person:
             mobile_no = get_contact_number(
                 doc.contact_person, 'Customer', doc.customer,
             )
             template = frappe.db.get_value(
                 'Gym Settings', None, 'sms_invoiced'
-            )
-            frequency = frappe.db.get_value(
-                'Subscription', doc.subscription, 'frequency'
             )
             text = get_sms_text(
                 template,
--- psd_customization/doc_events/subscription.py
+++ psd_customization/doc_events/subscription.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import add_days
 
 from psd_customization.fitness_world.doctype.gym_membership.gym_membership \
     import status_map
+from psd_customization.fitness_world.api.gym_membership \
+    import get_membership_by_subscription
 
 
 def on_submit(doc, method):
@@ -20,11 +21,6 @@ def on_submit(doc, method):
 
 def on_update_after_submit(doc, method):
     if doc.reference_gym_member:
-        membership_name = frappe.db.get_value(
-            'Gym Membership', filters={'subscription': doc.name}
-        )
-        if membership_name:
-            membership = frappe.get_doc('Gym Membership', membership_name)
-            membership.status = status_map.get(doc.status, membership.status)
-            membership.expiry_date = add_days(doc.next_schedule_date, -1)
-            membership.save()
+        membership = get_membership_by_subscription(doc.name)
+        membership.status = status_map.get(doc.status, membership.status)
+        membership.save()
