878c640de83790b6111ffad715e224fca7883754
Sun Howwrongbum <sun@libermatic.com>
2018-09-10 02:35:09 +0530

    Remove unused code

--- psd_customization/doc_events/sales_invoice.py
+++ psd_customization/doc_events/sales_invoice.py
@@ -1,60 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
-from frappe.utils import formatdate
-from frappe.core.doctype.sms_settings.sms_settings \
-    import get_contact_number, send_sms
+
 from psd_customization.fitness_world.api.gym_fee import get_fee_by_invoice
 
-from psd_customization.fitness_world.doctype.gym_sms_template.\
-    gym_sms_template import get_sms_text
-from psd_customization.fitness_world.doctype.gym_membership.\
-    gym_membership import get_expiry_date, is_gym_customer
-from psd_customization.fitness_world.api.gym_membership \
-    import get_membership_by_subscription
 
-
-def _make_template_data(doc, frequency):
-    end_date = get_expiry_date(doc.posting_date, frequency)
-    period = doc.get_formatted('posting_date') \
-        if doc.posting_date == end_date else \
-        '{} - {}'.format(
-            doc.get_formatted('posting_date'), formatdate(end_date)
-        )
-    return {
-        'customer_name': doc.customer_name,
-        'sales_invoice': doc.name,
-        'amount': doc.get_formatted('rounded_total'),
-        'period': period,
-        'due_date': doc.get_formatted('due_date'),
-    }
-
-
-def on_submit(doc, method):
-    if is_gym_customer(doc.customer) and doc.subscription:
-        membership = get_membership_by_subscription(doc.subscription)
-        frequency = frappe.db.get_value(
-            'Subscription', doc.subscription, 'frequency'
-        )
-        if doc.status == 'Paid' and membership:
-            membership.update_expiry_date(
-                get_expiry_date(doc.posting_date, frequency)
-            )
-
-        if doc.contact_person:
-            mobile_no = get_contact_number(
-                doc.contact_person, 'Customer', doc.customer,
-            )
-            template = frappe.db.get_value(
-                'Gym Settings', None, 'sms_invoiced'
-            )
-            text = get_sms_text(
-                template,
-                _make_template_data(doc, frequency),
-            )
-            if mobile_no and text:
-                # fix for json.loads casting to int during number validation
-                send_sms('"{}"'.format(mobile_no), text)
 def on_cancel(doc, method):
     fee = get_fee_by_invoice(doc.name)
     if fee:
--- psd_customization/fitness_world/doctype/gym_membership/gym_membership.py
+++ psd_customization/fitness_world/doctype/gym_membership/gym_membership.py
@@ -3,9 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe.model.document import Document
-from frappe.utils import add_days, add_months, add_years, date_diff
+from frappe.utils import date_diff
 
 
 status_map = {
@@ -17,64 +16,10 @@ status_map = {
 
 
 class GymMembership(Document):
-    def before_submit(self):
-        subscription = frappe.get_doc('Subscription', self.subscription)
-        if subscription.docstatus != 1:
-            return frappe.throw(
-                'Subscription {} has a status of {}'.format(
-                    self.subscription, subscription.status
-                )
-            )
-        self.status = status_map.get(subscription.status)
-        self.start_date = subscription.start_date
-        self.expiry_date = get_expiry_date(
-            subscription.start_date, subscription.frequency
-        )
-        self.end_date = subscription.end_date
-        self.frequency = subscription.frequency
-        self.items = []
-        si = frappe.get_doc('Sales Invoice', subscription.reference_document)
-        if si:
-            for item in si.items:
-                self.append('items', {
-                    'item_code': item.item_code,
-                    'item_name': item.item_name,
-                })
-
-    def before_cancel(self):
-        self.status = 'Cancelled'
-
-    def on_cancel(self):
-        subscription = frappe.get_doc('Subscription', self.subscription)
-        subscription.cancel()
+    def onload(self):
+        self.set_onload('current_outstanding', 100)
 
     def update_expiry_date(self, expiry_date, force=False):
         if force or date_diff(expiry_date, self.expiry_date) > 0:
             self.expiry_date = expiry_date
             self.save()
-
-
-def get_expiry_date(posting_date, frequency):
-    if frequency == 'Daily':
-        return posting_date
-    if frequency == 'Weekly':
-        return add_days(posting_date, 6)
-    if frequency == 'Monthly':
-        return add_days(add_months(posting_date, 1), -1)
-    if frequency == 'Quaterly':
-        return add_days(add_months(posting_date, 3), -1)
-    if frequency == 'Half-Yearly':
-        return add_days(add_months(posting_date, 6), -1)
-    if frequency == 'Yearly':
-        return add_years(posting_date, 1)
-    return None
-
-
-def is_gym_customer(customer):
-    gym_member_group = frappe.db.get_value(
-        'Gym Settings', None, 'default_customer_group'
-    )
-    customer_group = frappe.db.get_value(
-        'Customer', customer, 'customer_group'
-    )
-    return gym_member_group == customer_group
