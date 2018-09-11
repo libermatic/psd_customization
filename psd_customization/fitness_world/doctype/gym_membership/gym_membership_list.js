e7b2379703a6bc07cd2f8e13391afac55c7938cb
Sun Howwrongbum <sun@libermatic.com>
2018-09-11 21:29:16 +0530

    Gym Membership: set status indicators

---
+++ psd_customization/fitness_world/doctype/gym_membership/gym_membership_list.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2018, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings['Gym Membership'] = {
+  add_fields: ['status'],
+  get_indicator: function({ status }) {
+    if (status === 'Active') {
+      return [status, 'green', 'status,=,Active'];
+    }
+    if (status === 'Stopped' || status === 'Expired') {
+      return [status, 'red', `status,=,${status}`];
+    }
+  },
+};
