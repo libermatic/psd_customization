a269e5c8929cfa72ec50ac1d6b7bcc541a249266
Sun Howwrongbum <sun@libermatic.com>
2018-09-23 22:47:55 +0530

    Settings: setup query for sms template fields

--- psd_customization/fitness_world/doctype/gym_settings/gym_settings.js
+++ psd_customization/fitness_world/doctype/gym_settings/gym_settings.js
@@ -2,6 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gym Settings', {
+  setup: function(frm) {
+    frm.trigger('set_queries');
+  },
+  set_queries: async function(frm) {
+    ['sms_invoiced', 'sms_overdue', 'sms_receipt'].forEach(field => {
+      frm.set_query(field, () => ({ filters: { type: 'Transactional' } }));
+    });
+  },
   refresh: async function(frm) {
     const { message: options } = await frappe.call({
       method: 'erpnext.accounts.doctype.pos_profile.pos_profile.get_series',
