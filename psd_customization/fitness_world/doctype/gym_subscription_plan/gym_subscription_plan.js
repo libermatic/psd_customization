9f67f9879b9f2ca87a35f539c16a6339912c46cd
Sun Howwrongbum <sun@libermatic.com>
2018-10-10 01:37:11 +0530

    Subscription Plan: add Pricing Rule

--- psd_customization/fitness_world/doctype/gym_subscription_plan/gym_subscription_plan.js
+++ psd_customization/fitness_world/doctype/gym_subscription_plan/gym_subscription_plan.js
@@ -10,19 +10,24 @@ frappe.ui.form.on('Gym Subscription Plan', {
       item_code: async function(frm, cdt, cdn) {
         const { item_code } = frappe.get_doc(cdt, cdn) || {};
         if (item_code) {
+          frappe.model.set_value(cdt, cdn, 'qty', 1);
+        } else {
+          frappe.model.set_value(cdt, cdn, 'qty', 0);
+        }
+      },
+      qty: async function(frm, cdt, cdn) {
+        const { item_code, qty = 0, rate = 0 } = frappe.get_doc(cdt, cdn) || {};
+        if (qty) {
+          const { item_code } = frappe.get_doc(cdt, cdn) || {};
           const { message: price } = await frappe.call({
             method:
               'psd_customization.fitness_world.api.gym_subscription.get_item_price',
-            args: { item_code },
+            args: { item_code, qty, no_pricing_rule: 0 },
           });
           frappe.model.set_value(cdt, cdn, 'rate', price);
-          frappe.model.set_value(cdt, cdn, 'qty', 1);
         } else {
           frappe.model.set_value(cdt, cdn, 'rate', 0);
         }
-      },
-      qty: function(frm, cdt, cdn) {
-        const { qty = 0, rate = 0 } = frappe.get_doc(cdt, cdn) || {};
         frappe.model.set_value(cdt, cdn, 'amount', qty * rate);
       },
       rate: function(frm, cdt, cdn) {
