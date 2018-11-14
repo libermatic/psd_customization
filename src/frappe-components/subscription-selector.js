e192a7bab57171c13af8d9fb77d1d84a661398d3
Sun Howwrongbum <sun@libermatic.com>
2018-11-14 23:30:57 +0530

    SubscriptionSelector: create this and use in Sales Invoice

---
+++ src/frappe-components/subscription-selector.js
@@ -0,0 +1,101 @@
+// Copyright (c) 2018, Libermatic and contributors
+// For license information, please see license.txt
+
+class SubscriptionDialog {
+  constructor() {
+    this.dialog = new frappe.ui.Dialog({
+      title: 'Select Subscription Period',
+      fields: [
+        { fieldname: 'ht', fieldtype: 'HTML' },
+        { fieldtype: 'Section Break' },
+        {
+          label: 'Frequency',
+          fieldname: 'frequency',
+          fieldtype: 'Select',
+          options: 'Monthly\nQuarterly\nHalf-Yearly\nYearly\nLifetime',
+          default: 'Monthly',
+        },
+        { fieldtype: 'Column Break' },
+        {
+          label: 'From Date',
+          fieldname: 'from_date',
+          fieldtype: 'Date',
+          default: frappe.datetime.nowdate(),
+        },
+        {
+          label: 'To Date',
+          fieldname: 'to_date',
+          fieldtype: 'Date',
+          default: frappe.datetime.nowdate(),
+        },
+      ],
+    });
+    this._setup();
+  }
+  _setup() {
+    const months = {
+      Monthly: 1,
+      Quarterly: 3,
+      'Half-Yearly': 6,
+      Yearly: 12,
+      Lifetime: 60,
+    };
+    const handle_to_date = () => {
+      const { frequency, from_date } = this.dialog.get_values();
+      if (from_date && frequency && months[frequency]) {
+        if (frequency === 'Lifetime') {
+          this.dialog.set_value('to_date', null);
+          this.dialog.fields_dict['to_date'].toggle(false);
+        } else {
+          this.dialog.fields_dict['to_date'].toggle(true);
+          this.dialog.set_value(
+            'to_date',
+            frappe.datetime.add_days(
+              frappe.datetime.add_months(from_date, months[frequency]),
+              -1
+            )
+          );
+        }
+      }
+    };
+    this.dialog.fields_dict['frequency'].$input.on('input', handle_to_date);
+    this.dialog.fields_dict['from_date'].$input.on('blur', handle_to_date);
+  }
+  async register(frm, cdt, cdn) {
+    const row = frappe.get_doc(cdt, cdn) || {};
+    if (!row.item_code) {
+      return false;
+    }
+    const { message: sub_item } = await frappe.call({
+      method:
+        'psd_customization.fitness_world.api.gym_subscription_item.get_subscription_item',
+      args: { item_code: row.item_code },
+    });
+    if (!sub_item) {
+      return false;
+    }
+    const { item_name } = sub_item;
+    if (item_name) {
+      this.dialog.set_title(`Select Period for ${item_name}`);
+    }
+    this.dialog.set_primary_action('OK', () => {
+      const { frequency, from_date, to_date } = this.dialog.get_values();
+      frappe.model.set_value(cdt, cdn, 'is_gym_subscription', 1);
+      if (frequency === 'Lifetime') {
+        frappe.model.set_value(cdt, cdn, 'gym_is_lifetime', 1);
+        frappe.model.set_value(cdt, cdn, 'gym_from_date', from_date);
+      } else {
+        frappe.model.set_value(cdt, cdn, 'gym_from_date', from_date);
+        frappe.model.set_value(cdt, cdn, 'gym_to_date', to_date);
+      }
+      this.dialog.hide();
+      this.dialog.get_primary_btn().off('click');
+    });
+    return true;
+  }
+  show() {
+    this.dialog.show();
+  }
+}
+
+export default SubscriptionDialog;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 import TrainingSchedule from './TrainingSchedule.vue';
+import components from './frappe-components';
 
 export default {
   make_training_schedule_page: node =>
@@ -8,4 +9,5 @@ export default {
       el: node,
       render: h => h(TrainingSchedule),
     }),
+  components,
 };
