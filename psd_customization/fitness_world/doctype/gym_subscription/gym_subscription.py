7ce8af9ac42fad5d34e4a38119441dc8805473e5
Sun Howwrongbum <sun@libermatic.com>
2018-11-19 00:31:50 +0530

    Subscription: remove unused methods

--- psd_customization/fitness_world/doctype/gym_subscription/gym_subscription.py
+++ psd_customization/fitness_world/doctype/gym_subscription/gym_subscription.py
@@ -4,36 +4,11 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cint, flt, getdate, add_months, add_days
+from frappe.utils import cint, getdate
 from frappe.model.document import Document
-from functools import reduce, partial
-from toolz import pluck, compose, get
-from copy import deepcopy
 
-from psd_customization.fitness_world.api.gym_membership import (
-    get_membership_by,
-)
-from psd_customization.fitness_world.api.gym_subscription import (
-    dispatch_sms, make_sales_invoice,
-    get_existing_subscription, has_valid_subscription,
-)
-from psd_customization.utils.datetime import month_diff
-
-months = {
-    'Monthly': 1,
-    'Quarterly': 3,
-    'Half-Yearly': 6,
-    'Yearly': 12,
-}
-
-
-def _update_item_qty(qty):
-    def fn(item):
-        r = deepcopy(item)
-        r.qty = flt(qty)
-        r.amount = r.qty * flt(r.rate)
-        return r
-    return fn
+from psd_customization.fitness_world.api.gym_subscription \
+    import validate_dependencies
 
 
 class GymSubscription(Document):
@@ -49,182 +24,57 @@ class GymSubscription(Document):
             })
 
     def validate(self):
-        if not self.membership_items and not self.service_items:
-            frappe.throw('Cannot create Subscription without any items')
-        if not self.membership_items and \
-                not get_membership_by(
-                    self.member, self.from_date, self.to_date
-                ):
-            frappe.throw('Cannot create Subscription without Membership')
         self.validate_dates()
-        self.validate_items()
-        if self.service_items:
-            self.validate_service_dependencies()
+        self.validate_item()
+        if self.flags.source_doc != 'Sales Invoice':
+            validate_dependencies(
+                self.member,
+                [frappe._dict({
+                    'item_code': self.subscription_item,
+                    'item_name': self.subscription_name,
+                    'from_date': self.from_date,
+                    'to_date': self.to_date,
+                    'is_lifetime': self.is_lifetime,
+                })]
+            )
 
     def validate_dates(self):
+        if not cint(self.is_lifetime):
+            if not self.from_date or not self.to_date:
+                frappe.throw('Both dates are required')
+            if getdate(self.from_date) > getdate(self.to_date):
+                frappe.throw('From date cannot be after to date')
+        if not self.from_date:
+            frappe.throw('Start Date cannot be empty')
+
+    def validate_item(self):
         if cint(self.is_lifetime):
-            if not self.from_date:
-                frappe.throw('Start Date cannot be empty')
-        elif not self.from_date or not self.to_date:
-            frappe.throw('Both dates are required')
-        elif getdate(self.from_date) >= getdate(self.to_date):
-            frappe.throw('From date cannot be the same or after to date')
-
-    def validate_items(self):
-        items = frappe.get_all(
-            'Item',
-            filters={
-                'item_group': frappe.db.get_value(
-                    'Gym Settings', None, 'default_item_group',
-                ),
-            },
-            fields=[
-                'name',
-                'is_gym_membership_item',
-                'is_gym_subscription_item',
-                'can_be_lifetime'
-            ]
-        )
-
-        def filter_and_get(key):
-            return compose(
-                partial(pluck, 'name'),
-                partial(filter, lambda x: x.get(key))
+            can_be_lifetime = frappe.db.get_value(
+                'Gym Subscription Item',
+                self.subscription_item,
+                'can_be_lifetime',
             )
-
-        for item in self.membership_items:
-            if item.item_code \
-                    not in filter_and_get('is_gym_membership_item')(items):
-                frappe.throw(
-                    'Invalid item(s) in Membership Item table. Please remove '
-                    'them and try again.'
-                )
-        for item in self.service_items:
-            if item.item_code \
-                    not in filter_and_get('is_gym_subscription_item')(items):
-                frappe.throw(
-                    'Invalid item(s) in Subscription Item table. Please '
-                    'remove them and try again.'
-                )
-            if cint(self.is_lifetime) and item.item_code \
-                    not in filter_and_get('can_be_lifetime')(items):
-                frappe.throw(
-                    'Subscription cannot be created for non lifetime items.'
-                )
-
-    def validate_service_dependencies(self):
-        subscription_exists = partial(
-            get_existing_subscription,
-            member=self.member,
-            start_date=self.from_date,
-            end_date=self.to_date,
-            lifetime=cint(self.is_lifetime),
-        )
-        dependency_exists = partial(
-            has_valid_subscription,
-            member=self.member,
-            start_date=self.from_date,
-            end_date=self.to_date,
-            lifetime=cint(self.is_lifetime),
-        )
-        for item in self.service_items:
-            existing = subscription_exists(item_code=item.item_code)
-            if existing:
-                frappe.throw(
-                    'Another Subscription - {subscription}, for {item_code}'
-                    ' already exists during this time frame.'.format(
-                        subscription=existing.get('subscription'),
-                        item_code=item.item_name
+            if not cint(can_be_lifetime):
+                return frappe.throw(
+                    'Subscription Item {} cannot be Lifetime'.format(
+                        self.subscription_item_name
                     )
                 )
-            for p in pluck(
-                'item',
-                frappe.get_all(
-                    'Gym Item Parent',
-                    fields=['item'],
-                    filters={
-                        'parent': item.item_code,
-                        'parentfield': 'gym_parent_items',
-                        'parenttype': 'Item',
-                    }
-                ),
-            ):
-                if p not in map(lambda x: x.item_code, self.service_items) \
-                        and not dependency_exists(item_code=p):
-                    p_name = frappe.db.get_value('Item', p, 'item_name')
-                    frappe.throw(
-                        'Required dependency {} not fulfiled.'.format(p_name)
-                    )
 
     def before_save(self):
-        if cint(self.is_opening):
-            self.status = 'Paid'
-
-        get_to_date = compose(
-            getdate,
-            partial(add_days, days=-1),
-            partial(add_months, self.from_date),
-            partial(get, seq=months, default=0),
-        )
-        if getdate(self.to_date) != get_to_date(self.frequency):
-            self.frequency = None
-
-        if cint(self.is_opening) or frappe.flags.in_import:
-            self.membership_items = map(
-                _update_item_qty(1), self.membership_items
-            )
-            set_qty = compose(_update_item_qty, month_diff)(
-                self.from_date, self.to_date, as_dec=1
-            )
-            self.service_items = map(set_qty, self.service_items)
-
-        self.total_amount = reduce(
-            lambda a, x: a + flt(x.amount),
-            self.membership_items + self.service_items,
-            0
-        )
-
-    def on_submit(self):
-        if self.membership:
-            membership = frappe.get_doc('Gym Membership', self.membership)
-            if membership:
-                membership.reference_doc = self.name
-                membership.status = 'Active' if self.status == 'Paid' else None
-                membership.save()
-        if not cint(self.no_invoice):
-            self.reference_invoice = self.create_sales_invoice()
-
-    def on_update_after_submit(self):
-        if self.status == 'Paid':
-            dispatch_sms(self.name, 'sms_receipt')
-        if self.membership:
-            membership = frappe.get_doc('Gym Membership', self.membership)
-            if membership:
-                membership.status = 'Active' if self.status == 'Paid' else None
-                membership.save()
+        if cint(self.is_lifetime):
+            self.to_date = None
 
     def before_cancel(self):
         if self.reference_invoice:
-            si = frappe.get_doc('Sales Invoice', self.reference_invoice)
-            if si and si.docstatus == 1:
-                si.cancel()
-
-    def on_cancel(self):
-        if self.membership:
-            membership = frappe.get_doc('Gym Membership', self.membership)
-            if membership:
-                membership.reference_doc = None
-                membership.status = None
-                membership.save()
-
-    def create_sales_invoice(self):
-        si = make_sales_invoice(self.name)
-        si.set_posting_time = 1
-        si.posting_date = self.posting_date
-        si.due_date = None
-        si.payment_terms_template = frappe.db.get_value(
-            'Gym Settings', None, 'default_payment_template'
-        )
-        si.insert()
-        si.submit()
-        return si.name
+            row_item_name = frappe.db.exists('Sales Invoice Item', {
+                'parent': self.reference_invoice,
+                'gym_subscription': self.name,
+            })
+            if row_item_name:
+                frappe.db.set_value(
+                    'Sales Invoice Item',
+                    row_item_name,
+                    'gym_subscription',
+                    None,
+                )
