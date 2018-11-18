d127878dbd5d7a7239bd8af2120b9661087370ba
Sun Howwrongbum <sun@libermatic.com>
2018-11-18 19:46:04 +0530

    SubscriptionDashboard: create this

---
+++ src/SubscriptionDashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <dashboard-item v-bind="invoice_cpt" />
+  </div>
+</template>
+
+<script>
+import DashboardItem from './components/DashboardItem.vue';
+
+function get_color(status) {
+  if (status === 'Paid') {
+    return 'green';
+  }
+  if (status === 'Unpaid') {
+    return 'orange';
+  }
+  if (status === 'Overdue') {
+    return 'red';
+  }
+  return 'blue';
+}
+
+export default {
+  props: ['invoice'],
+  components: { DashboardItem },
+  computed: {
+    invoice_cpt: function() {
+      const { amount, status } = this.invoice || {};
+      return {
+        label: 'Invoice Amount',
+        content: amount
+          ? format_currency(amount, frappe.defaults.get_default('currency'))
+          : '-',
+        color: get_color(status),
+      };
+    },
+  },
+};
+</script>
