06ddd6097ef9b569f77a0d91f648e5c7c5a38309
Sun Howwrongbum <sun@libermatic.com>
2018-11-20 03:49:41 +0530

    Move Gym Subscription scripts to JS bundle

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 
 import TrainingSchedule from './TrainingSchedule.vue';
-import SubscriptionDashboard from './SubscriptionDashboard.vue';
 import scripts from './scripts';
 import utils from './utils';
 
 export default {
-  make_subscription_dashboard: (node, props) =>
-    new Vue({
-      el: node,
-      render: h => h(SubscriptionDashboard, { props }),
-    }),
   make_training_schedule_page: node =>
     new Vue({
       el: node,
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,10 @@
 import { gym_member } from './gym_member';
+import { gym_subscription } from './gym_subscription';
 import { sales_invoice, sales_invoice_item } from './sales_invoice';
 
-export default { sales_invoice, sales_invoice_item, gym_member };
+export default {
+  sales_invoice,
+  sales_invoice_item,
+  gym_member,
+  gym_subscription,
+};
