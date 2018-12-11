977c79c15a00675a8393e36341b056ab6fb41ff7
Sun Howwrongbum <sun@libermatic.com>
2018-12-11 11:47:02 +0530

    Trainer Allocation: add validation to check for overlap
    - add field to hold trainer_name

--- psd_customization/fitness_world/doctype/trainer_allocation/trainer_allocation.py
+++ psd_customization/fitness_world/doctype/trainer_allocation/trainer_allocation.py
@@ -8,7 +8,31 @@ from frappe.model.document import Document
 
 
 class TrainerAllocation(Document):
+    def validate(self):
+        existing = frappe.db.sql(
+            """
+                SELECT 1 FROM `tabTrainer Allocation`
+                WHERE
+                    gym_subscription=%(gym_subscription)s AND
+                    from_date<=%(to_date)s AND
+                    to_date>=%(from_date)s
+            """,
+            values={
+                'gym_subscription': self.gym_subscription,
+                'from_date': self.from_date,
+                'to_date': self.to_date,
+            },
+            debug=1,
+        )
+        if existing:
+            frappe.throw(
+                'Another allocation already exists during this time frame.'
+            )
+
     def before_save(self):
-        self.gym_member = frappe.db.get_valye(
-            'Gym Subscription', self.gym_subscription, 'member'
+        self.gym_member = frappe.db.get_value(
+            'Gym Subscription', self.gym_subscription, 'member',
+        )
+        self.gym_trainer_name = frappe.db.get_value(
+            'Gym Trainer', self.gym_trainer, 'trainer_name',
         )
